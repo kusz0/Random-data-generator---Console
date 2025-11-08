606579f38f5360163a4c76a8eb201dd18fc2e315
Kacper Kuszek <kuszekkacper@gmail.com>
2025-11-08 04:07:36 +0100

    Display class added and main class done

---
+++ Random Data Generator/Display.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Data_Generator
+{
+    internal class Display
+    {
+
+
+        public void Print()
+        {
+            Console.WriteLine("===========Random ID Generator=============");
+
+            Console.Write("Write Id length:");
+            bool isValidNumber = int.TryParse(Console.ReadLine(), out int idLength);
+            while(!isValidNumber)
+            {
+                Console.WriteLine("Invalid number!!! Try Again");
+                Console.Write("Write Id length: ");
+                isValidNumber = int.TryParse(Console.ReadLine(), out idLength);
+            } 
+            RandomDataGen randomDataGen = new RandomDataGen(idLength);
+
+            string randomID = randomDataGen.RandomID();
+            Console.Clear();
+            Console.WriteLine("===========Random ID Generator=============");
+            Console.WriteLine("Your random id: ");
+            Console.Write(randomID);
+        }
+
+    }
+}
--- Random Data Generator/Program.cs
+++ Random Data Generator/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            
+            Display display = new Display();    
+            display.Print();
         
         }
 
