5abe953b8d6ff9f33ed1227339d39ff89d1e593a
Kacper Kuszek <kuszekkacper@gmail.com>
2025-11-08 03:00:26 +0100

    Inital commit

---
+++ Random Data Generator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Random_Data_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
